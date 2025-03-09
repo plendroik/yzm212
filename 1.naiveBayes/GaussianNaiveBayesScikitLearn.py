2992dfcf5b3b40b00703e302b788b162847b68a3
plendroik <yigit.temel61l@gmail.com>
2025-03-09 14:21:39 +0300

    ilk commit

---
+++ 1.naiveBayes/GaussianNaiveBayesScikitLearn.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import time
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+dataFrame = pd.read_csv("1.naiveBayes\yzm212\heart.csv")
+
+X = dataFrame.drop(columns=["target"])
+y = dataFrame["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+nbModel = GaussianNB()
+
+baslangic = time.time()
+nbModel.fit(X_train, y_train)
+egitimSuresi = time.time() - baslangic
+
+baslangic = time.time()
+predictions = nbModel.predict(X_test)
+testSuresi = time.time() - baslangic
+
+accuracy = accuracy_score(y_test, predictions)
+print(f"Gaussian NB Model Doğruluğu: {accuracy:.2f}")
+print(f"Eğitim süresi: {egitimSuresi:.4f} saniye")
+print(f"Test süresi: {testSuresi:.4f} saniye")
+
+karmasasiklikMatrisi = confusion_matrix(y_test, predictions)
+
+plt.figure(figsize=(6, 4))
+sns.heatmap(karmasasiklikMatrisi, annot=True, fmt="d", cmap="Blues", xticklabels=["Sağlıklı", "Hasta"], yticklabels=["Sağlıklı", "Hasta"])
+plt.xlabel("Tahmin")
+plt.ylabel("Gerçek")
+plt.title("Karmaşıklık Matrisi")
+plt.show()
